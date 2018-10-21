21fccd1f82ea575e91dcbd22f71d9afffd5a4afe
vksh <vikashkumar.sh45@gmail.com>
2018-10-21 15:06:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Persons from '../src/components/persons/persons.component';
 class App extends Component {
-  persons = [
-    {
-      name: 'Vikash',
-      age: '22',
-      role: 'Developer'
-    },
-    {
-      name: 'Lionel',
-      age: '32',
-      role: 'Footballer'
-    },
-    {
-      name: 'Tim Cook',
-      age: '42',
-      role: 'CEO'
-    },
-    {
-      name: 'Ronaldo',
-      age: '33',
-      role: 'Footballer'
-    }
-  ]
   render() {
     return (
       <div className="App">
-        <Persons persons={this.persons}></Persons>
+        <h1>React App</h1>
       </div>
     );
   }
